0c70b7d583fe49943e2a60c0a10e64c8f237f770
Danil Mosolov <koti4ch@gmail.com>
2025-01-13 15:30:05 +0500

    [issue/refresh] perf: optimization Refresh()

--- heartbeat.go
+++ heartbeat.go
@@ -3,12 +3,15 @@ package heartbeat
 import (
 	"log"
 	"os"
+	"sync/atomic"
 	"time"
 )
 
 const defaultFilePath = "/tmp/healthy"
 
 var filePath string
+
+var timestamp = new(atomic.Int64) // second.
 
 // init initializes the global file path using the default file path or environment variable.
 func init() {
@@ -22,6 +25,11 @@ func init() {
 // Refresh creates or updates the timestamp file with the current time.
 // It skips calls if refreshed within the last second.
 func Refresh() {
+	current := time.Now().Local()
+	if timestamp.Load() == current.Unix() {
+		return // skip.
+	}
+
 	_, err := os.Stat(filePath)
 	switch {
 	case os.IsNotExist(err):
@@ -34,10 +42,10 @@ func Refresh() {
 	case err != nil:
 		log.Printf("heartbeat: [ERROR] Heartbeat Refresh failed: could not create or open file '%s': %v", filePath, err)
 	default:
-		current := time.Now().Local()
 		err = os.Chtimes(filePath, current, current)
 		if err != nil {
 			log.Printf("heartbeat: [ERROR] Heartbeat Refresh failed: could not modification times '%s': %v", filePath, err)
 		}
+		timestamp.Store(current.Unix())
 	}
 }
